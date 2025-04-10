d5d69e0c32533785f54751fab7516507d8b55cbc
anahi-ortiz <anahi.ortiz@ucb.edu.bo>
2025-04-10 09:48:46 -0400

    Add files via upload

---
+++ Operador_aritmetico.py
@@ -0,0 +1,40 @@
+
+#crear una var
+saludo = "hola mundo!!"
+print(saludo)
+
+
+#suma de numeros
+num1 = 10
+num2 = 20
+suma = num1 + num2
+
+print(suma)
+
+#suma palabras -> concatenar palabras
+
+nume1 = "hola "
+nume2 = "mundo!!"
+suma_2 = nume1 + nume2
+
+print(suma_2)
+
+#resta
+resta1 = 10
+resta2 = 15
+resta_total = resta1 -resta2
+print(resta_total)
+
+#multiplicacion
+
+multi1 = 5
+multi2 = 8
+multiplicacion = multi1 * multi2
+print(multiplicacion)
+
+#division
+div1 = 150
+div2 = 3
+division = (int(div1 / div2))
+print(division)
+
